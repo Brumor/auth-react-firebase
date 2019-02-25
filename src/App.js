e0221b48fb6deeb5e9e4282a04a21b8638ea8e05
Brumor <paul.bricout@factorymarket.com>
2019-02-25 16:50:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+
+class App extends Component {
+  render () {
+    return (
+      <View style={{flex:1, backgroundColor: '#FFF'}}>
+        <Text> An App </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/common/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80,
+    paddingTop: 25,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export default Header;
